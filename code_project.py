68c736fe28df501d6d52d5959a0c90cb0aebe098
JaneAirVlad <124638379+JaneAirVlad@users.noreply.github.com>
2025-03-11 15:41:25 +0300

    Update code_project.py

--- code_project.py
+++ code_project.py
@@ -1,56 +1,59 @@
 import random
 
-digits = '0123456789'                               # подключение строковых констант
+# Строковые константы
+digits = '0123456789'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
-                                          
-def data_collection():                              # функция сбора условий генерации пароля
+
+def get_input_from_file():
+    with open('input_data.txt', 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file.readlines()]
+
+def data_collection(inputs):                             
     global quantity, lenght, symbol, chars
-    chars = ''                        
-    quantity = int(input('Количестве паролей для генерации? (Целое число)\n'))
-    lenght = int(input('Длина одного пароля? (Целое число)\n'))
-    if quantity == 0 or lenght== 0:
+    chars = ''
+    quantity = int(inputs[0])  # Количество паролей
+    lenght = int(inputs[1])    # Длина пароля
+    if quantity == 0 or lenght == 0:
         print('Неверное количество паролей или их длина!')
-        restart()
-    c = input('Включать ли цифры? (Да = включать, Нет = не включать)\n')
-    if c.lower() == 'да':
+        return
+
+    # Обработка включения различных символов
+    if inputs[2].lower() == 'да':
         chars += digits
-    d = input('Включать ли прописные буквы? (Да = включать, Нет = не включать)\n')
-    if d.lower() == 'да':
+    if inputs[3].lower() == 'да':
         chars += lowercase_letters   
-    e = input('Включать ли строчные буквы? (Да = включать, Нет = не включать)\n')
-    if e.lower() == 'да':
+    if inputs[4].lower() == 'да':
         chars += uppercase_letters    
-    f = input('Включать ли символы !#$%&*+-=?@^_? (Да = включать, Нет = не включать)\n')
-    if f.lower() == 'да':
+    if inputs[5].lower() == 'да':
         chars += punctuation 
-    symbol = input('Исключать ли неоднозначные символы il1Lo0O? (Да = исключать, Нет = не исключать)\n')
+    symbol = inputs[6]  # Исключать ли неоднозначные символы
+  
     if chars == '':
         print('Вы неправильно сформировали запрос для пароля. Попробуйте еще раз!')
-        restart()
+        return
 
-def generate_password(lenght, quantity, chars):         # функция генерации пароля
+def generate_password(lenght, quantity, chars):
     password = ''
     for i in range(quantity):
         num = lenght
         while num > 0:
             password += random.choice(chars)
             num -= 1
-        if symbol.lower() == 'да':                      # проверка на неоднозначные символы
+        if symbol.lower() == 'да':
             for c in 'il1Lo0O':
                 chars = chars.replace(c, '')
         print(password)
         num = lenght
         password = ''
 
-def restart():                                          # функция запуска кода и повтора
-    again = 'да'
-    while again.lower() == 'да':
-        data_collection()
-        generate_password(lenght, quantity, chars)    
-        again = input('Вам еще нужны пароли ? (Да = запустить еще раз, Любая клавиша = выйти из программы) \n' )
-        
+def restart():
+    inputs = get_input_from_file()
+    data_collection(inputs)
+    if chars:  # Если символы неправильные - не генерировать пароли
+        generate_password(lenght, quantity, chars)
+
 print('Добро пожаловать в генератор паролей. Ответьте на несколько вопросов для создания пароля.')
 restart()
-print('Всегда рады Вам помочь! Досвидания.')
+print('Всегда рады Вам помочь! До свидания.')
